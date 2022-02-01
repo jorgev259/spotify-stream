fd83de4dfd19ff7b6b24711be132af4fc16d0eaa
jorgev259 <jorgev259@outlook.es>
2022-02-01 13:43:59 -0600

    Handle auth errors gracefully

--- spotifyApi.js
+++ spotifyApi.js
@@ -3,14 +3,17 @@ import fs from 'fs-extra'
 
 const { redirectUri, clientId } = process.env
 
-const accessCode = fs.existsSync('./.secret') && fs.readFileSync('./.secret', 'utf-8')
-
+export let code = fs.existsSync('./.secret') && fs.readFileSync('./.secret', 'utf-8')
 const spotifyApi = new SpotifyWebApi({ redirectUri, clientId })
 
-if (accessCode) spotifyApi.setAccessToken(accessCode)
+if (code) spotifyApi.setAccessToken(code)
 
 export default spotifyApi
 export async function getCurrentSong () {
   const data = await spotifyApi.getMyCurrentPlayingTrack()
   return data.body
+}
+export function setCode (accessCode) {
+  code = accessCode
+  spotifyApi.setAccessToken(accessCode)
 }
